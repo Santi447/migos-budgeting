54ab031ab88f28164e9490beb664c9f45e8b9cca
Santiago pabon <santiago.pabon72@gmail.com>
2026-04-07 11:11:19 -0600

    Update summaryCardSection.jsx

--- components/dashboardComponents/summaryCardSection.jsx
+++ components/dashboardComponents/summaryCardSection.jsx
@@ -24,10 +24,13 @@ export default function SummaryCardSection() {
   return(
     <div>
       <div>
-
+      <SummaryCard title={data[0].title} amount={data[0].amount} reportValue={data[0].reportValue} />
       </div>
       <div>
-
+      <SummaryCard title={data[1].title} amount={data[1].amount} reportValue={data[1].reportValue} />
+      </div>
+      <div>
+      <SummaryCard title={data[2].title} amount={data[2].amount} reportValue={data[2].reportValue} />
       </div>
       <div>
 
