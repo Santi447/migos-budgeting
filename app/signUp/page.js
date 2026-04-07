8a27549c8b7d8e744e68332ba3bc0755b6a89b8a
Santiago pabon <santiago.pabon72@gmail.com>
2026-04-06 19:07:44 -0600

    created signup page

--- app/signUp/page.js
+++ app/signUp/page.js
@@ -1,12 +1,19 @@
 import SignupForm from "@/components/signupFormComponents/signupForm"
 import HomeFooter from "@/components/homeFooter"
+import SignupFormBackground from "@/components/signupFormComponents/signupFormBackground"
 export default function Page() {
   return(
     <main className="flex min-h-screen flex-col bg-[#fbf9f8] border-2 border-red-500">
-    <div className="flex border-2 border-blue-500 flex-col items-center gap-6 px-8 py-12">
-      <SignupForm />
+    <div className="flex flex-1 border-2 border-blue-500 overflow-hidden">
+      <div className="relative flex-1 overflow-hidden">
+        <SignupFormBackground />
+      </div>
+      <div className="flex w-full max-w-[460px] items-center justify-center bg-[#fbf9f8] px-8 py-12">
+        <SignupForm />
+      </div>
     </div>
     <HomeFooter />
+    
     </main>
   )
 }
--- components/signupFormComponents/signupFormBackground.jsx
+++ components/signupFormComponents/signupFormBackground.jsx
@@ -2,8 +2,14 @@ import Image from "next/image";
 
 export default function SignupFormBackground() {
   return (
-    <Image>
-      
-    </Image>
-  )
+    <div className="absolute inset-0 z-0 overflow-hidden">
+      <Image
+        src="/signupBg.svg"
+        alt="Signup Background"
+        fill
+        className="object-cover"
+        sizes="100vw"
+      />
+    </div>
+  );
 }
