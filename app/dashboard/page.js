e239d909e6cfc3ad67c4e7137dcc887d72b160e8
Santiago pabon <santiago.pabon72@gmail.com>
2026-04-06 23:02:15 -0600

    created dashboard header

---
+++ app/dashboard/page.js
@@ -0,0 +1,14 @@
+import DashboardHeader from "../../components/dashboardComponents/dashboardHeader";
+export default function Page(){
+  return(
+    <main>
+      <DashboardHeader />
+      <h2>
+        Daily Summary
+      </h2>
+      <p>
+        Tracking your organic financial footprint.
+      </p>
+    </main>
+  );
+}
---
+++ components/dashboardComponents/dashboardHeader.jsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import {LogOut} from 'lucide-react';
+export default function Dashboard() {
+  return(
+    <div className="w-full bg-[#fbf9f8]">
+      <nav className="flex h-[54px] w-full items-center justify-between px-8">
+        <div className="flex items-center gap-8">
+          <h1 className="font-sans font-extrabold text-3xl font-bold text-[#64463D]">
+            Migo&apos;s Budgeting
+          </h1>
+
+          <span
+            className="font-[manrope] border-b-2 border-[#64463D] px-3 pt-2 pb-1 font-semibold text-[#64463D]"
+          >
+            Dashboard
+          </span>
+        </div>
+        <div className="flex items-center gap-3">
+          <Link
+            href="/loginPage"
+            className="font-[manrope] flex h-10 w-25 items-center justify-center rounded-lg px-3 py-2 text-[14px] font-semibold text-[#111111] transition-colors hover:bg-black/5"
+          >
+            <LogOut />
+          </Link>
+        </div>
+      </nav>
+    </div>
+  );
+}
